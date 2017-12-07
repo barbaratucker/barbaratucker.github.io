8576cb6468899c4cc71fe119ffb5de6177fa59fb
Chloe Shiau <chloe.shiau@gmail.com>
2017-12-06 21:41:17 -0800

    change soap

--- js/lightbox.js
+++ js/lightbox.js
@@ -509,3 +509,12 @@
 
   return new Lightbox();
 }));
+
+
+$(document).on('click', 'a[href^="#"]', function (event) {
+    event.preventDefault();
+
+    $('html, body').animate({
+        scrollTop: $($.attr(this, 'href')).offset().top
+    }, 500);
+});
